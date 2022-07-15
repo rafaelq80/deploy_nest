62a44ebe1f7458717f406dd0451f8d23450af43e
Rafael <rafaelproinfo@gmail.com>
2022-07-15 00:47:46 -0300

    Teste

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -23,10 +23,10 @@ export class AuthService {
       }
     
       async login(user: any) {
-        const payload = { username: user.username, sub: user.userId };     
+        const payload = { username: user.usuario, sub: user.userId };     
       
         return {
-          usuario: user.username,
+          usuario: user.usuario,
           access_token: this.jwtService.sign(payload),
         };
       }
